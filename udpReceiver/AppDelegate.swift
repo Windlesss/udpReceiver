e89abeaa5df8a79308023d955c2b756e05f5c302
王天元 <windless.d@gmail.com>
2016-04-12 16:49:54 +0800

    Initial Commit

---
+++ udpReceiver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  udpReceiver
+//
+//  Created by 王天元 on 16/4/12.
+//  Copyright © 2016年 王天元. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ udpReceiver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  udpReceiver
+//
+//  Created by 王天元 on 16/4/12.
+//  Copyright © 2016年 王天元. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
